4a7c874e27081d9e95e0951e7994bb4fa5bba8ec
agrant312 <ahg4@students.ptcollege.edu>
2023-05-16 18:25:06 -0400

    Created Collaborator Class

---
+++ Collab/src/Collaborator.java
@@ -0,0 +1,20 @@
+public class Collaborator {
+    public String name;
+    public int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
